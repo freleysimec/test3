7b926374cd46cddab12f877db0ca481b76f040cd
Fre Leys (----) <leys40@imec.be>
2023-03-07 16:19:17 +0100

    venv

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def main():
-    print("Hello test3!")
-    print("Hello new branch 1!")
+
+    # Generate random data
+    x = np.random.randn(100)
+    y = np.random.randn(100)
+
+    # Create scatter plot
+    plt.scatter(x, y)
+
+    # Add labels and title
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Random Scatter Plot')
+
+    # Display plot
+    plt.show()
+
+
 
 
 if __name__=='__main__':
